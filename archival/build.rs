95fbd767b8e6d068bed46de8c2faf4155e5f963a
juh9870 <juh9870@ukr.net>
2023-10-05 15:58:18 +0300

    attempt 5

--- archival/build.rs
+++ archival/build.rs
@@ -1,9 +1,18 @@
+use std::{path::Path, fs};
+
 use npm_rs::{NodeEnv, NpmEnv};
 
 fn main() {
     println!("cargo:rerun-if-changed=./archive_viewer/src");
     println!("cargo:rerun-if-changed=./archive_viewer/package.json");
     println!("cargo:rerun-if-changed=./archive_viewer/build-config.mjs");
+    let ci_file: &Path = "./archive_viewer/ci/archive.html".as_ref();
+    if ci_file.exists() {
+        fs::create_dir_all("./archive_viewer/dist").expect("Failed to create dist directory for CI file");
+        // ci_file
+        fs::rename(ci_file, "./archive_viewer/dist/archive.html").expect("Failed to move CI file to dist location");
+        return
+    }
     let built = NpmEnv::default()
         .with_node_env(&NodeEnv::Development)
         .set_path("./archive_viewer")
